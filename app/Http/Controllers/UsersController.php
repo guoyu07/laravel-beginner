2e03145d59ac31750d98f0e25e7251430847bd85
huliuqing <huliuqing1989@gmail.com>
2018-03-28 23:36:54 +0800

    用户注册模块 - 用户头像信息显示

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,13 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
+    public function index()
+    {
+        return view('users.index');
+    }
+
     public function create()
     {
         $title = '注册';
 
         return view('users.create', compact('title'));
+    }
+
+    public function show(User $user)
+    {
+        $user->gravatar();
+        $title = '用户信息';
+        return view('users.show', compact(['title','user']));
+    }
+
+    public function store(Request $request)
+    {
+        return redirect()->back();
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        return redirect()->back();
+    }
+
+    public function destroy(User $user)
+    {
+        return redirect()->back();
     }
 }
